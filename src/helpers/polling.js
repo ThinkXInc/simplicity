acd278808602b0f101832230bc9887ffbacbeb39
kazukiotsuka <otsuka.kazuki@googlemail.com>
2023-10-13 18:20:09 +0900

    polling: fix expectedValue null case in polling

--- src/helpers/polling.js
+++ src/helpers/polling.js
@@ -61,7 +61,7 @@ class PollingManager {
         // If the empty string is allowed, change to `expectedValue == null` 
         // and we need to disable the "reqired" option in title.
         // The 7B model sometimes miss title.
-        if (!expectedValue) { // check if it exists or is non-empty
+        if (expectedValue == null) { // check if it exists or is non-empty
             if (responseData.success && responseData.success.message || responseData.code == 202) {
                 console.log("Still waiting for processing...");
                 return false;
